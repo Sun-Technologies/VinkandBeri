1f9770c3c4eb8bbb5af9387ba1ef8578005c1e5d
chandramouleshwar <chandra.shalwi@gmail.com>
2014-08-14 07:42:33 -0400

    store locator demo content removed

--- beta/store-locator.php
+++ beta/store-locator.php
@@ -27,13 +27,7 @@
       <article class="content span12">
 		<div class="row">
 		  <div class="span12">
-			<p>We believe that every company has a social responsibility they must fulfill and work towards achieving. At Vink &amp; Beri, we are passionate about social care and always endeavor to contribute to society.
-       As manufacturers and marketers of natural food products, we aim to creatively offer natural, nutritious and amazing items that are produced in an environmentally sustainable and socially responsible way.</p>
-      <p>We emphasize a great deal on the quality of our products and believe in honesty and fair pricing. We are committed to excellence and this factor clearly reflects across all Vink &amp; Beri products.</p>
-      <p>The principles that guide us in delivering the best in social care include transparency, sincerity and fairness. We are always striving hard to initiate new social activities that create value for our
-       consumers as well as society at large.</p>
-      <p>Vink &amp; Beri will donate 2% of its overall profit towards social calls from benefit of American children. The same details will be available shortly on this website.</p>
-      <p>At Vink &amp; Beri, we believe in giving - and giving generously - so much that it makes a real difference. Our grant helps in our vision and creates value in the life of a child who mostly needs the support.</p>
+			
 		  </div>
 		</div> <br><br>
 	  </article><!-- .content -->
