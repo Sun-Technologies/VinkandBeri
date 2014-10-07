80f1f09f5c3b0cb0112ac676ab676d1addcb3728
chandramouleshwar <chandra.shalwi@gmail.com>
2014-10-07 05:13:52 -0400

    store location get location fixed

--- beta/js/map.js
+++ beta/js/map.js
@@ -133,7 +133,7 @@
       var enname =  htmlEntities(name);
 
       var jscript = 'javascript:showMarker("' + lat + '","' + lng + '", "' + enname + '", " ' + address + '")';
-      option.innerHTML = "<div id=location-area>" + "<a href='" + jscript + "'><b>" + name + "(" + distance.toFixed(1) + " mi)"  + "</b></a><br/>" + address + "<br/><a href='http://maps.google.com/?q="+name+"' target='_blank' id='get-location'>Get Directions <i class=icon-arrow-right></i></a>" + "</div>";
+      option.innerHTML = "<div id=location-area>" + "<a href='" + jscript + "'><b>" + name + "(" + distance.toFixed(1) + " mi)"  + "</b></a><br/>" + address + "<br/><a href='https://maps.google.com/?saddr="+addressInput.value+"&daddr="+enname+ "+"+address+"' target='_blank' id='get-location'>Get Directions <i class=icon-arrow-right></i></a>" + "</div>";
       locationSelect.appendChild(option);
     }
 
